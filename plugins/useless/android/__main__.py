94eb5803cbaec52430e5600868f7bca00e481482
ᴠᴇxɪᴅᴏ <103483308+vexido@users.noreply.github.com>
2022-04-29 06:43:26 +0700

    ok

--- plugins/useless/android/__main__.py
+++ plugins/useless/android/__main__.py
@@ -6,8 +6,16 @@
 
 """for stuff related to android"""
 
-from .. import android
+from typing import Optional
+
+import ujson
+from bs4 import BeautifulSoup
+from requests import get
 from userge import Message, userge
+
+magisk_release = (
+    "https://raw.githubusercontent.com/topjohnwu/magisk-files/master/{}.json"
+)
 
 
 @userge.on_cmd(
