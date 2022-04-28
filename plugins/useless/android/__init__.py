d5968d07b618bd6aebb494a5f7fadeffdef1f86f
ᴠᴇxɪᴅᴏ <103483308+vexido@users.noreply.github.com>
2022-04-29 06:25:25 +0700

    Update __init__.py

--- plugins/useless/android/__init__.py
+++ plugins/useless/android/__init__.py
@@ -7,6 +7,15 @@ from bs4 import BeautifulSoup
 from requests import get
 from userge import Message, userge
 
+
 magisk_release = (
     "https://raw.githubusercontent.com/topjohnwu/magisk-files/master/{}.json"
 )
+
+
+class Dynamic:
+    TIMEOUT = 60
+
+
+def shared_method() -> None:
+    pass
